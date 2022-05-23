957f9a0836d4bb9ed9db36143055f3d68e1ff56f
Henning <henning44@gmx.net>
2022-05-23 14:56:52 +0200

    Added counter output for different failure classification.

--- src/test/TestUtils.ts
+++ src/test/TestUtils.ts
@@ -137,16 +137,32 @@ export class TestUtils {
             }
         }
 
-        // TODO: specific results
-
+        //get specific failure classes
         if (failure.length > 0) {
+
+            let failureClassArray: any[] = [];
+            for (let result in actualData.fileResults)
+            {
+                failureClassArray.push(actualData.fileResults[result].mainClass);
+            }
+
+            const failureClassMap = new Map();
+            for (let i = 0; i < failureClassArray.length; i++) {
+                let item = failureClassArray[i];
+
+                failureClassMap.has(item) ? failureClassMap.set(item, failureClassMap.get(item) + 1) : failureClassMap.set(item, 1)
+            }
+
+            console.log(failureClassMap);
             console.error("Something went wrong while validating input. Failed properties: " + failure);
             let message: string = "";
             for (let pos in failure) {
                 let entry = failure[pos];
                 message = message + `Property '${entry}'\n    Expected: '${expected[entry]}'\n    actual: '${actualData[entry]}'\n`;
             }
+
             throw new ValidationError(message);
+
         }
         console.log("Successfully validated.")
     }
