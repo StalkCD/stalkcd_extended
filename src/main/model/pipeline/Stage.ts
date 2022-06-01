3f6e698915199cc9898f3340d5c8b8624abacfea
Clemens Kurz <ClemensB.Kurz@gmail.com>
2022-06-01 07:13:59 +0200

    added setters for Stage.ts

--- src/main/model/pipeline/Stage.ts
+++ src/main/model/pipeline/Stage.ts
@@ -4,6 +4,13 @@ import { IPostSection, PostSection } from "./PostSection";
 import { IAgentOption } from "./AgentSection";
 import { IEnvironmentVariable } from "./EnvironmentSection";
 
+/**
+ * This Interface represents two concepts in the meta-model.
+ * 1. Stages --> this is handled in the constructor where the stages are mapped.
+ * 2. Stage --> if no Stage-Children are present in the stages attribute.
+ *
+ * This Concept is rather confusing and is prone to misuse.
+ */
 export interface IStage {
 
     name: string;
@@ -16,7 +23,7 @@ export interface IStage {
     options?: string[];
     tools?: string[];
     input?: string[];
-    when?: string;
+    when?: string; // TODO: corresponding to the meta-model this should be string[]
     parallel?: boolean;
     steps?: IStep[] ;
     stages?: IStage[];
@@ -32,69 +39,98 @@ export class Stage extends PositionAwareObject<IStage> {
         super();
         Object.assign(this, init);
         if (init.stages) {
-            this.stages = init.stages.map(s => new Stage(s));
+            this._stages = init.stages.map(s => new Stage(s));
         }
         if (init.steps) {
-            this.steps = init.steps.map(s => Step.fromSerial(s));
+            this._steps = init.steps.map(s => Step.fromSerial(s));
         }
         if (init.post) {
-            this.post = PostSection.fromSerial(init.post);
+            this._post = PostSection.fromSerial(init.post);
         }
 
         this.savePropertyPosition(init);
     }
 
-    name: string = "";
-    baseName?: string;
-
-    failFast?: boolean;
-
-    isAutoStage: boolean = false;
-
-    parallel: boolean = false;
-
-    parent?: Stage;
-
-    agent?: IAgentOption[];
-
-    environment?: IEnvironmentVariable[];
-
-    options?: string[];
-
-    tools?: string[];
-
-    input?: string[];
-    
-    when?: string;
-
-    steps?: Step[];
-
-    stages?: Stage[];
-
-    post: PostSection = new PostSection();
+    private _name: string = "";
+    private _baseName?: string;
+    private _failFast?: boolean;
+    private _parallel: boolean = false;
+    private _agent?: IAgentOption[];
+    private _environment?: IEnvironmentVariable[];
+    private _options?: string[];
+    private _tools?: string[];
+    private _input?: string[];
+    private _when?: string;
+    private _steps?: Step[];
+    private _stages?: Stage[];
+    private _post: PostSection = new PostSection();
 
 
     get isEmpty(): boolean {
-        return (!this.steps || this.steps.length === 0)
-            || (!this.stages || this.stages.length === 0);
+        return (!this._steps || this._steps.length === 0)
+            || (!this._stages || this._stages.length === 0);
     }
 
     toSerial(): any {
         const res: IStage = {
-            name: this.name,
-            failFast: this.failFast,
-            baseName: this.baseName,
-            agent: clean(this.agent),
-            environment: clean(this.environment),
-            options: clean(this.options),
-            tools: clean(this.tools),
-            input: this.input,
-            when: this.when,
-            parallel: this.parallel ? true : undefined,
-            steps: this.steps ? this.steps.map(s => s.toSerial()) : undefined,
-            stages: this.stages ? this.stages.map(s => s.toSerial()) : undefined,
-            post: this.post.toSerial(),
+            name: this._name,
+            failFast: this._failFast,
+            baseName: this._baseName,
+            agent: clean(this._agent),
+            environment: clean(this._environment),
+            options: clean(this._options),
+            tools: clean(this._tools),
+            input: this._input,
+            when: this._when,
+            parallel: this._parallel ? true : undefined,
+            steps: this._steps ? this._steps.map(s => s.toSerial()) : undefined,
+            stages: this._stages ? this._stages.map(s => s.toSerial()) : undefined,
+            post: this._post.toSerial(),
         };
         return clean(sortObject(res, this.propertiesOrder));
+    }
+
+    set baseName(value: string) {
+        this._baseName = value;
+    }
+
+    set failFast(value: boolean) {
+        this._failFast = value;
+    }
+
+    set parallel(value: boolean) {
+        this._parallel = value;
+    }
+
+    set agent(value: IAgentOption[]) {
+        this._agent = value;
+    }
+
+    set environment(value: IEnvironmentVariable[]) {
+        this._environment = value;
+    }
+
+    set options(value: string[]) {
+        this._options = value;
+    }
+
+    set tools(value: string[]) {
+        this._tools = value;
+    }
+
+    set input(value: string[]) {
+        this._input = value;
+    }
+
+    set when(value: string) {
+        this._when = value;
+    }
+
+    set steps(value: Step[]) {
+        this._steps = value;
+    }
+
+    set post(value: PostSection) {
+        this._post = value;
     }
 }
