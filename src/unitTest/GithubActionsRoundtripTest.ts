d1be46f55c3d82843870e4adf849b64631a9ecd2
Clemens Kurz <ClemensB.Kurz@gmail.com>
2022-08-20 18:28:56 +0200

    Better Roundtrip-Test-Support with more GH-Workflows.

--- src/unitTest/GithubActionsRoundtripTest.ts
+++ src/unitTest/GithubActionsRoundtripTest.ts
@@ -4,33 +4,49 @@ import {GithubWorkflowGenerator} from "../main/model/GitHubActions/GitHubWorkflo
 import {JsonSchemaValidator} from "../main/JsonSchemaValidator";
 import * as fs from "fs";
 import {Comparator} from "../main/Comparator";
-import * as yaml from "js-yaml";
-import {GithubWorkflow} from "../main/model/GitHubActions/GeneratedTypes";
+import {GithubActions2StalkCdEvaluation} from "../test/github/GithubActions2StalkCdEvaluation";
+import {AssertionError} from "./Asserts";
 
-const githubActionsFileParser = new GithubActionsFileParser(false);
+const githubActionsFileParser = new GithubActionsFileParser(false, ["StepWorkingDirectory"]);
+
+const ROUNDTRIP_TEST_FOLDER: string | Buffer | URL = "testRes/GithubRoundtrip/";
 
 export function parseData(filename: string): Pipeline  {
     // @ts-ignore
-    return githubActionsFileParser.parse("testRes/GithubRoundtrip/" + filename);
+    return githubActionsFileParser.parse(ROUNDTRIP_TEST_FOLDER + filename);
 }
 
-let expected = <GithubWorkflow>yaml.load(fs.readFileSync("testRes/GithubRoundtrip/main.yml", {encoding: 'utf8'}));
+function roundtripTest(filename: string ): void {
+    let expected = GithubActions2StalkCdEvaluation.loadFile(ROUNDTRIP_TEST_FOLDER + filename);
 
-let pipeline: Pipeline = parseData("main.yml");
+    let pipeline: Pipeline = parseData(filename);
 
-let generator: GithubWorkflowGenerator = new GithubWorkflowGenerator(true);
-let actual = generator.run(pipeline);
-let jsonResultString: string = JSON.stringify(actual);
-console.log("-----------------")
-console.log(jsonResultString)
-console.log("-----------------")
-fs.writeFileSync("testfile.json", jsonResultString)
+    let generator: GithubWorkflowGenerator = new GithubWorkflowGenerator(true);
+    let actual = generator.run(pipeline);
+    let jsonResultString: string = JSON.stringify(actual);
+    console.log("-----------------")
+    console.log(jsonResultString)
+    console.log("-----------------")
+    fs.writeFileSync("testfile.json", jsonResultString)
 
-new JsonSchemaValidator(GithubActionsFileParser.GITHUB_WORKFLOW_SCHEMA_PATH).validate("testfile.json")
-let map = Comparator.compareObjects(expected,actual);
-if (map.size > 0) {
-    console.log(map)
-    console.log("test failed");
-} else {
-    console.log("tested successfully");
+    try {
+        new JsonSchemaValidator(GithubActionsFileParser.GITHUB_WORKFLOW_SCHEMA_PATH).validate("testfile.json");
+    } catch (err) {
+        console.log("--> schema validation failed " + filename )
+        throw err
+    }
+
+    let map = Comparator.compareObjects(expected, actual);
+    if (map.size > 0) {
+        console.log(map)
+        console.log("--> deep comparison failed: " + filename);
+        throw new AssertionError("--> deep comparison failed: " + filename + "\n" + map);
+    } else {
+        console.log("tested successfully: " + filename);
+    }
 }
+
+roundtripTest("main.yml");
+roundtripTest("WopsS_RED4ext.SDK.build.yml");
+roundtripTest("actions-cool_issue-vote.test-all.yml");
+roundtripTest("active-group_reacl-c-testing.tests.yml");
