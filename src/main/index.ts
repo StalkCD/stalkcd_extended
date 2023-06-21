5a7825c1d20c890d076caa15683e35238c26d251
Piet Bohn <piet.bohn@web.de>
2023-06-21 23:05:38 +0200

    refactoring of server plus docker

---
+++ src/main/index.ts
@@ -0,0 +1,22 @@
+import { Application, Request, Response } from "express";
+import express = require("express");
+import morgan =  require("morgan");
+import swaggerUI = require("swagger-ui-express");
+
+import Router from "./routes";
+
+const PORT = process.env.PORT || 8000;
+
+const app: Application = express();
+
+app.use(express.json());
+app.use(morgan("tiny"));
+app.use(express.static("public"));
+
+app.use("/docs", swaggerUI.serve, swaggerUI.setup(undefined, { swaggerOptions: { url: "/swagger.json" } }))
+
+app.use(Router);
+
+app.listen(PORT, () => {
+    console.log("Server is running on port", PORT);
+});
---
+++ src/main/routes/index.ts
@@ -0,0 +1,12 @@
+import PingController from "../controllers/ping";
+import express = require("express");
+
+const router = express.Router();
+
+router.get("/ping", async (_req, res) => {
+    const controller = new PingController();
+    const response = await controller.getMessage();
+    return res.send(response);
+});
+
+export default router;
